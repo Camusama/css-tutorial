e189531f24cff4e46b0c0ae5ab7057680fd5914c
Camusama <a1012401749@163.com>
2018-04-13 19:12:45 +0800

    1

--- js-task/js-task2/桌游精灵/js/vote.js
+++ js-task/js-task2/桌游精灵/js/vote.js
@@ -2,7 +2,7 @@
 * @Author: Marte
 * @Date:   2018-04-12 18:01:32
 * @Last Modified by:   Marte
-* @Last Modified time: 2018-04-12 18:02:00
+* @Last Modified time: 2018-04-12 23:17:32
 */
 
 $(document).ready(function(){
